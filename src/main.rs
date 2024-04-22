b096fbbe420e74a46e394861ef3c6245326013a5
ThiccTheo <th3o1237@gmail.com>
2024-04-21 20:22:14 -0700

    this is the best i'm gonna get lol

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    input::mouse::MouseWheel,
-    prelude::*,
-    render::{
-        render_asset::RenderAssetUsages,
-        render_resource::{Extent3d, TextureDimension, TextureFormat},
+use {
+    bevy::{
+        diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+        input::mouse::MouseWheel,
+        prelude::*,
+        render::{
+            render_asset::RenderAssetUsages,
+            render_resource::{Extent3d, TextureDimension, TextureFormat},
+        },
+        window::{WindowMode, WindowResolution},
     },
-    window::WindowResolution,
+    rayon::prelude::*,
 };
 
 const WIDTH: usize = 1280;
@@ -22,6 +25,7 @@ fn main() {
                 primary_window: Some(Window {
                     resolution: WindowResolution::new(WIDTH as f32, HEIGHT as f32),
                     title: String::from("Mandelbrot Set"),
+                    mode: WindowMode::SizedFullscreen,
                     ..default()
                 }),
                 ..default()
@@ -31,7 +35,10 @@ fn main() {
         ))
         .insert_resource(ClearColor(OUT_SET))
         .add_systems(Startup, (spawn_cameras, spawn_visual))
-        .add_systems(Update, (adjust_view_of_visual, color_pixels_in_viewport).chain())
+        .add_systems(
+            Update,
+            (adjust_view_of_visual, color_pixels_in_viewport).chain(),
+        )
         .run();
 }
 
@@ -53,7 +60,7 @@ fn spawn_cameras(mut cmds: Commands) {
 fn adjust_view_of_visual(
     mut cam_qry: Query<(&mut Transform, &mut OrthographicProjection), With<CalculationsCamera>>,
     mut scroll_wheel_evr: EventReader<MouseWheel>,
-    kb: Res<ButtonInput<KeyCode>>
+    kb: Res<ButtonInput<KeyCode>>,
 ) {
     let (mut cam_xform, mut cam_projection) = cam_qry.single_mut();
 
@@ -126,10 +133,11 @@ fn color_pixels_in_viewport(
     let Some(img) = imgs.get_mut(visual.0.clone_weak()) else {
         return;
     };
-    let mut pixels = img.data.as_mut_slice().chunks_mut(4);
 
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
+    img.data = (0..WIDTH * HEIGHT)
+        .into_par_iter()
+        .map(|i| (i % WIDTH, i / WIDTH))
+        .map(|(x, y)| {
             let c = cam
                 .viewport_to_world_2d(cam_glob_xform, Vec2::new(x as f32, y as f32))
                 .unwrap();
@@ -146,14 +154,13 @@ fn color_pixels_in_viewport(
             }
             let rate = successful_iterations as f32 / ITERATIONS as f32;
 
-            let color = pixels.next().unwrap();
-            (color[0], color[1], color[2], color[3]) = Color::rgb(
+            Color::rgb(
                 f32::lerp(OUT_SET.r(), IN_SET.r(), rate),
                 f32::lerp(OUT_SET.g(), IN_SET.g(), rate),
                 f32::lerp(OUT_SET.b(), IN_SET.b(), rate),
             )
             .as_rgba_u8()
-            .into();
-        }
-    }
+        })
+        .flatten()
+        .collect();
 }
